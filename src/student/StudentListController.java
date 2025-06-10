5502319e091112b83823484db1d6964b9a4d35f9
m_fukano <m_fukano@LAPTOP-5G3KQ7D0>
2025-06-10 11:54:43 +0900

    0610 fukano 名前のみ

---
+++ src/student/StudentListController.java
@@ -0,0 +1,22 @@
+package student;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.CommonServlet;
+
+public class StudentListController extends CommonServlet {
+
+	@Override
+	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/subject/SubjectUpdateController.java
@@ -0,0 +1,22 @@
+package subject;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.CommonServlet;
+
+public class SubjectUpdateController extends CommonServlet {
+
+	@Override
+	protected void get(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
