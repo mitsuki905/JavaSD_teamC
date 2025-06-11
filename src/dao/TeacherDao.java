7825d776bfca8198ae6fe0ef3ed8752646d6bd80
m_nakada <m_nakada@LAPTOP-MV7CROC0>
2025-06-11 10:26:21 +0900

    ｊｈｊｇｈｌｊｈｇｊｈｌ

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -29,27 +29,31 @@ public class TeacherDao extends DAO {
 			    school.setCd(rs1.getString("cd"));
 			    school.setName(rs1.getString("name"));
 			}
-
+			else{
+				return null;
+			}
 
 			/*データベースから名前が一致しているデータがあるか
 			 * 調べて一致した名前があればオブジェクトを作る。
 			 */
-			String sqlName = "SELECT * FROM TEACHER WHERE ID =?";
+			String sql = "SELECT * FROM TEACHER WHERE ID =?";
 
-			PreparedStatement st = con.prepareStatement(sqlName);
+			PreparedStatement st = con.prepareStatement(sql);
 			st.setString(1,id);
 			ResultSet rs = st.executeQuery();
 
-
 			if(rs.next()){
 
-
+				teacher = new Teacher();
+				teacher.setId(rs.getString("id"));
+				teacher.setPassword(rs.getString("password"));
+				teacher.setName(rs.getString("name"));
+				teacher.setSchool(school);
 
 			}
 			else{
-				//一致する名前がない場合の処理
+				return null;
 			}
-
 
 		} catch (Exception e) {
 			// TODO 自動生成された catch ブロック
@@ -62,51 +66,23 @@ public class TeacherDao extends DAO {
 // ログイン
 	public Teacher login(String id, String password){
 
-		Teacher teacher = null;
-		School school = null;
-
-		try {
-			Connection con = getConnection();
-
-			//データベースから学校情報を持ってきてSchoolオブジェクトを作る
-			//teacherオブジェクトを完成させるため
-			String sqlschool = "SELECT * FROM SCHOOL";
-
-			PreparedStatement st1 = con.prepareStatement(sqlschool);
-			ResultSet rs1 = st1.executeQuery();
-
-			if (rs1.next()) {
-			    school = new School();
-			    school.setCd(rs1.getString("cd"));
-			    school.setName(rs1.getString("name"));
-			}
-
-			// IDとPASSWORDが両方一致するデータを取ってくるSQL
-			String loginSql = "select ID,PASSWORD from TEACHER where ID = ? and PASSWORD = ?";
-
-			PreparedStatement st = con.prepareStatement(loginSql);
-			st.setString(1,id);
-			st.setString(2,password);
-			ResultSet rs = st.executeQuery();
-
-			if(rs.next()){
-
-				teacher = new Teacher();
-				teacher.setId(rs.getString("id"));
-				teacher.setPassword(rs.getString("password"));
-				teacher.setName(rs.getString("name"));
-				teacher.setSchool(school);
-
-			}
+		// teacherオブジェクトをget関数で作成
+		Teacher teacher = get(id);
+		// nullが返された場合loginできない
+		if(teacher == null){
 
 			return null;
-
-
-		} catch (Exception e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
 		}
+		else if(teacher.getPassword() == password){
 
+			return teacher;
+		}
+		else{
+			return null;
+		}
 	}
-
 }
+
+
+
+
