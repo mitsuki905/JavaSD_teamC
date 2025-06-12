8cf4d8f3a26de203cb4c3256cd1f857a9034f5fb
m_matsuo <m_matsuo@LAPTOP-3DO2OA3S>
2025-06-12 11:21:21 +0900

    ゆｆｙｔ

--- src/dao/SchoolDao.java
+++ src/dao/SchoolDao.java
@@ -5,29 +5,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import bean.School;
-import bean.Student;
 
 public class SchoolDao extends DAO {
 	//指定した学生の学生番号を取得する
 	public School get(String no){
 		School school = null;
-		Student student = null;
+
 
 		try{
 			Connection con = getConnection();
-			String sql = "SELECT * FROM STUDENT";
+			String sql = "SELECT * FROM SCHOOL WHERE CD = ?";
 
-			PreparedStatement st1 = con.prepareStatement(sql);
-			ResultSet rs1 = st1.executeQuery();
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1,no);
+			ResultSet rs = st.executeQuery();
 
-			if (rs1.next()) {
-			    student = new Student();
-			    student.setNo(rs1.getString("no"));
-			    student.setName(rs1.getString("name"));
-			    student.setEntYear(Integer.parseInt(rs1.getString("entYear")));
-			    student.setClassNum(rs1.getString("classNum").charAt(0));
-			    student.setisAttend(rs1.getBoolean("isAttend"));
-			    student.setSchool(school);
+			if (rs.next()) {
+			 school = new School();
+			 school.setCd(rs.getString("cd"));
+			 school.setName(rs.getString("name"));
 			}
 		}catch (Exception e){
 
