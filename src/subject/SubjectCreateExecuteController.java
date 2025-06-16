38b9b4faccff4d943c975a55204c20536498a936
m_fukano <m_fukano@LAPTOP-5G3KQ7D0>
2025-06-16 14:54:52 +0900

    fukano create完成

--- src/subject/SubjectCreateExecuteController.java
+++ src/subject/SubjectCreateExecuteController.java
@@ -31,15 +31,54 @@ public class SubjectCreateExecuteController extends CommonServlet {
 		System.out.println(cd);
 		System.out.println(name);
 
-//		beanを使って学生の情報をまとめる
-		Subject subject = new Subject();
-		subject.setSchool(school);
-		subject.setCd(cd);
-		subject.setName(name);
-
-//		まとめたデータに変更させる
+//		重複データがあるか検索する
 		SubjectDao dao = new SubjectDao();
-		boolean flag = dao.save(subject);
+		Subject choufuku = new Subject();
+		choufuku = dao.get(cd,school);
+
+//		重複がない場合新規登録をする
+		if (choufuku == null) {
+//			beanを使って学生の情報をまとめる
+			Subject subject = new Subject();
+			subject.setSchool(school);
+			subject.setCd(cd);
+			subject.setName(name);
+
+			boolean flag = dao.save(subject);
+
+//			データの変更に成功したかの判定式
+			if (flag != false) {
+				// messageに変更完了を渡す
+				req.setAttribute("message", "登録が完了しました");
+
+				// --- 変更完了ページへフォワード ---
+				req.getRequestDispatcher("subject_create_done.jsp").forward(req, resp);
+			}else {
+
+				// 認証失敗時：入力された科目名を再表示用にセット
+				req.setAttribute("cd", cd);
+	            req.setAttribute("name", name);
+
+	            // エラーメッセージをリクエストに追加
+	            req.setAttribute("errorMessage", "予期せぬエラーが発生しました");
+
+	            // ログイン画面に戻る
+	            req.getRequestDispatcher("subject_create.jsp").forward(req, resp);
+			}
+
+		} else {
+			// 認証失敗時：入力された科目名を再表示用にセット
+			req.setAttribute("cd", cd);
+            req.setAttribute("name", name);
+
+            // エラーメッセージをリクエストに追加
+            req.setAttribute("errorMessage", "科目コードが重複しています");
+
+         // ログイン画面に戻る
+            req.getRequestDispatcher("subject_create.jsp").forward(req, resp);
+		}
+
+
 
 	}
 
