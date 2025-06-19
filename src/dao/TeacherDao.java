4a31a5e2a42074056b797556c92aa1df071049a2
h_sae <h_sae@LAPTOP-LL21HKVI>
2025-06-19 11:40:08 +0900

    戻したよ

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -16,42 +16,37 @@ public class TeacherDao extends DAO {
 		School school = null;
 		SchoolDao dao = new SchoolDao();
 
+		/*データベースから名前が一致しているデータがあるか
+		 * 調べて一致した名前があればオブジェクトを作る。
+		 */
+		String sql = "SELECT * FROM TEACHER WHERE ID = ?";
 
-		try {
+		// try-with-resources構文を使用して、リソースが自動的にクローズされるように修正
+		try (
 			Connection con = getConnection();
-
-
-			/*データベースから名前が一致しているデータがあるか
-			 * 調べて一致した名前があればオブジェクトを作る。
-			 */
-			String sql = "SELECT * FROM TEACHER WHERE ID = ?";
-
-			PreparedStatement st = con.prepareStatement(sql);
+			PreparedStatement st = con.prepareStatement(sql)
+		) {
 			st.setString(1,id);
-			ResultSet rs = st.executeQuery();
 
-			if(rs.next()){
-
-				teacher = new Teacher();
-				teacher.setId(rs.getString("id"));
-				teacher.setPassword(rs.getString("password"));
-				teacher.setName(rs.getString("name"));
-				// teacherにあるschool_cdから学校をget関数でインスタンス化
-				school = dao.get(rs.getString("school_cd"));
-
-				teacher.setSchool(school);
-
-			}
-			else{
-				return null;
+			try (ResultSet rs = st.executeQuery()) {
+				if(rs.next()){
+					teacher = new Teacher();
+					teacher.setId(rs.getString("id"));
+					teacher.setPassword(rs.getString("password"));
+					teacher.setName(rs.getString("name"));
+					// teacherにあるschool_cdから学校をget関数でインスタンス化
+					school = dao.get(rs.getString("school_cd"));
+					teacher.setSchool(school);
+				}
+				// else節は不要。teacherがnullのまま返されるため
 			}
 
 		} catch (Exception e) {
 			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
+			// アプリケーション全体でエラーをハンドリングできるように、実行時例外をスローする
+			throw new RuntimeException("教員データの取得に失敗しました。", e);
 		}
 		return teacher;
-
 	}
 
 // ログイン
@@ -59,18 +54,14 @@ public class TeacherDao extends DAO {
 
 		// teacherオブジェクトをget関数で作成
 		Teacher teacher = get(id);
-		// nullが返された場合loginできない
-		if(teacher == null){
 
-			return null;
-		}
-		else if(teacher.getPassword().equals(password)){
-
-		// ｄｂ上にあるログインする人の情報を返す
+		// ログインロジックをよりシンプルに修正 (nullチェックとパスワードチェックを同時に行う)
+		if(teacher != null && teacher.getPassword().equals(password)){
+			// ｄｂ上にあるログインする人の情報を返す
 			return teacher;
 		}
-		else{
-			return null;
-		}
+
+		// 上記の条件に合わない場合はすべてnullを返す
+		return null;
 	}
 }
