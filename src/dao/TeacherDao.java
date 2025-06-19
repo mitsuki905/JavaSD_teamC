e50c2b7d5a27c541f37a605edc21d896467208f7
h_sae <h_sae@LAPTOP-LL21HKVI>
2025-06-19 11:33:53 +0900

    戻したよ

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -9,36 +9,68 @@ import bean.Teacher;
 
 public class TeacherDao extends DAO {
 
-    public Teacher get(String id) {
-        Teacher teacher = null;
-        SchoolDao schoolDao = new SchoolDao();
-        String sql = "SELECT * FROM TEACHER WHERE ID = ?";
+//  一致する名前があるか判定
+	public Teacher get(String id){
 
-        try (Connection con = getConnection();
-             PreparedStatement st = con.prepareStatement(sql)) {
+		Teacher teacher = null;
+		School school = null;
+		SchoolDao dao = new SchoolDao();
 
-            st.setString(1, id);
-            try (ResultSet rs = st.executeQuery()) {
-                if (rs.next()) {
-                    teacher = new Teacher();
-                    teacher.setId(rs.getString("id"));
-                    teacher.setPassword(rs.getString("password"));
-                    teacher.setName(rs.getString("name"));
-                    School school = schoolDao.get(rs.getString("school_cd"));
-                    teacher.setSchool(school);
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("教員データの取得に失敗しました。", e);
-        }
-        return teacher;
-    }
 
-    public Teacher login(String id, String password) {
-        Teacher teacher = get(id);
-        if (teacher != null && teacher.getPassword().equals(password)) {
-            return teacher;
-        }
-        return null;
-    }
+		try {
+			Connection con = getConnection();
+
+
+			/*データベースから名前が一致しているデータがあるか
+			 * 調べて一致した名前があればオブジェクトを作る。
+			 */
+			String sql = "SELECT * FROM TEACHER WHERE ID = ?";
+
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1,id);
+			ResultSet rs = st.executeQuery();
+
+			if(rs.next()){
+
+				teacher = new Teacher();
+				teacher.setId(rs.getString("id"));
+				teacher.setPassword(rs.getString("password"));
+				teacher.setName(rs.getString("name"));
+				// teacherにあるschool_cdから学校をget関数でインスタンス化
+				school = dao.get(rs.getString("school_cd"));
+
+				teacher.setSchool(school);
+
+			}
+			else{
+				return null;
+			}
+
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return teacher;
+
+	}
+
+// ログイン
+	public Teacher login(String id, String password){
+
+		// teacherオブジェクトをget関数で作成
+		Teacher teacher = get(id);
+		// nullが返された場合loginできない
+		if(teacher == null){
+
+			return null;
+		}
+		else if(teacher.getPassword().equals(password)){
+
+		// ｄｂ上にあるログインする人の情報を返す
+			return teacher;
+		}
+		else{
+			return null;
+		}
+	}
 }
